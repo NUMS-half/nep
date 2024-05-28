1d00af478cab02a92296453086b780db4c8cd71e
NUMS-half <1272606165@qq.com>
2024-05-28 09:38:35 +0800

    nep-common-service MVC completed

---
+++ eureka-server/src/test/java/com/neusoft/neu24/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neusoft.neu24.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
